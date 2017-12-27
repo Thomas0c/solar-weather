bb0e8b272a6d03572876551277f1466eba00b8ab
Thomas Carlson <thomas.oca@gmail.com>
2017-12-27 14:45:36 +0100

    Add additional snapshot tests for AlertContent, ContentModal and Empty

--- __tests__/Background.js
+++ __tests__/Background.js
@@ -3,7 +3,12 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Background from '../lib/js/app/components/background';
 
-test('renders correctly', () => {
-  const tree = renderer.create(<Background />).toJSON();
+test('renders correctly for clear-night', () => {
+  const tree = renderer.create(<Background condition="clear-night" day={false} />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('renders correctly for cloudy day', () => {
+  const tree = renderer.create(<Background condition="cloudy" day />).toJSON();
   expect(tree).toMatchSnapshot();
 });
---
+++ __tests__/Empty.js
@@ -0,0 +1,9 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Empty from '../lib/js/app/components/empty';
+
+test('renders correctly', () => {
+  const tree = renderer.create(<Empty />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
